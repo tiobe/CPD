bc541c4a1fc505d166b7362f65e578fa3d389bd9
brouwers <122355335+brouwers-tiobe@users.noreply.github.com>
2024-10-15 14:17:04 +0200

    35202: Indicate form-feed presence in test file

--- pmd-matlab/src/test/resources/net/sourceforge/pmd/lang/matlab/cpd/testdata/formFeed.m
+++ pmd-matlab/src/test/resources/net/sourceforge/pmd/lang/matlab/cpd/testdata/formFeed.m
@@ -1,7 +1,7 @@
 classdef LC
     methods
         function [obj, c,t, s ] = Classification( obj,m,t, cm )%#codegen
-        end
+        end % This line contains a form-feed character (U+000C)
     end
 end
 
