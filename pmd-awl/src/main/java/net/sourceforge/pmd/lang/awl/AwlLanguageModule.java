a7130269c457027c5ed9042ba6638caeb0c2dad4
brouwers <122355335+brouwers-tiobe@users.noreply.github.com>
2025-06-17 12:10:22 +0200

    RM-36465: Add AWL support to CPD

---
+++ pmd-awl/src/main/java/net/sourceforge/pmd/lang/awl/AwlLanguageModule.java
@@ -0,0 +1,23 @@
+/*
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
+package net.sourceforge.pmd.lang.awl;
+
+import net.sourceforge.pmd.cpd.CpdLexer;
+import net.sourceforge.pmd.lang.LanguagePropertyBundle;
+import net.sourceforge.pmd.lang.awl.cpd.AwlCpdLexer;
+import net.sourceforge.pmd.lang.impl.CpdOnlyLanguageModuleBase;
+
+public class AwlLanguageModule extends CpdOnlyLanguageModuleBase {
+    private static final String ID = "awl";
+
+    public AwlLanguageModule() {
+        super(LanguageMetadata.withId(ID).name("AWL").extensions("awl"));
+    }
+
+    @Override
+    public CpdLexer createCpdLexer(LanguagePropertyBundle bundle) {
+        return new AwlCpdLexer();
+    }
+}
